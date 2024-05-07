fcd72fe5593b3943e590f74742d718a1378db3bf
lorxr <lorenco.zambiazi@gmail.com>
2024-05-07 12:55:58 -0300

    Criado construtor da classe Administrador

--- src/br/com/lavagest/modelo/entidade/Administrador.java
+++ src/br/com/lavagest/modelo/entidade/Administrador.java
@@ -1,5 +1,17 @@
 package br.com.lavagest.modelo.entidade;
 
-public class Administrador {
+import java.io.Serializable;
+
+public class Administrador extends Usuario implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public Administrador() {
+		
+	}
+	
+	public Administrador (String nome, String sobrenome, String telefone, String cpf, String senha) {
+		super(nome, sobrenome, telefone, cpf, senha);
+	}
 
 }
